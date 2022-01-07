f3e8f65b2cd8b82c648d6ab97d4391b0a4872628
adrienmaurer <adrienmaurer@hotmail.com>
2022-01-07 10:41:38 +0100

    modification des composants

--- monAppAngular/src/app/app.component.ts
+++ monAppAngular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'monAppAngular';
+  texte = 'monAppAngular';
+texte1 = 'bonjour les loulous ';
 }
